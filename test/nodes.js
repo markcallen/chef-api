102ad6b52a0c7dda2cf42f48e59a6729cbb1fd34
Mark Allen <mark@markcallen.com>
2014-02-14 08:48:23 -0800

    Added vagrant and berks to create a chef server running in a vm.  Added mocha for testing.

---
+++ test/nodes.js
@@ -0,0 +1,46 @@
+var expect = require("../node_modules/expect.js");
+
+var chef = require('./setup').chef;
+
+describe('nodes', function() {
+  before(function(done){
+    var node = {
+      "name": "test-node-1",
+      "run_list": [
+      ],
+      "normal": {
+        "tags": [
+        ]
+      }
+    };
+    
+    chef.createNode(node, function(err, res) {
+      if (err) 
+        throw err;
+
+      expect(res.uri).to.eql("https://33.33.33.50/nodes/test-node-1");
+      done();
+    });
+  });
+
+  describe('getNodes', function(){
+    it('get all nodes', function(done){
+      chef.getNodes(function(err, res){
+        if(err)
+          done(err);
+  
+        expect(res['test-node-1']).to.eql("https://33.33.33.50/nodes/test-node-1");
+        done();
+      });
+    });
+  });
+
+  after(function(done) {
+    chef.deleteNode("test-node-1", function(err, res) {
+      if (err)
+        done(err);
+
+      done();
+    });
+  });
+});
---
+++ test/setup.js
@@ -0,0 +1,14 @@
+var options = {
+    user_name: "admin",
+    key_path: "./admin.pem",
+    url: "https://33.33.33.50",
+    ca: null
+};
+
+var ChefApi = require("../main");
+var chef = new ChefApi();
+chef.config(options);
+
+module.exports = {
+  chef: chef
+}
